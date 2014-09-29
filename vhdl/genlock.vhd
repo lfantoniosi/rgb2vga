9ef09de2b37800de47c068754bff7bae15608841
lfantoniosi <felipe.antoniosi@gmail.com>
2014-09-29 12:50:24 -0400

    improve DAC error detection

--- vhdl/genlock.vhd
+++ vhdl/genlock.vhd
@@ -9,10 +9,9 @@ entity genlock is
 	);
 
     port(CLOCK_H : in std_logic;
-			CLOCK_V : in std_logic;
-			VSYNC_IN  : in std_logic;
-			HSYNC_IN	 : in std_logic;
-			ARGB	 : in	unsigned(2 downto 0); -- analog r, g, b
+			VSYNC_IN  : in std_logic; -- digital vsync
+			HSYNC_IN	 : in std_logic; -- digital hsync
+			ARGB	 	 : in	unsigned(2 downto 0); -- analog r, g, b
 			
 			pixelOut		: buffer unsigned(15 downto 0);
 			rowStoreNr	: out unsigned(8 downto 0); 
@@ -37,13 +36,172 @@ signal GADC : unsigned(6 downto 0);
 signal BADC : unsigned(6 downto 0);
 signal PIXEL : unsigned(7 downto 0);
 
+function F_ADC(ADC: unsigned) return unsigned;
+
+
+function F_ADC(ADC: unsigned) return unsigned is
+variable VALUE : unsigned (2 downto 0); 
+begin
+		case ADC(6 downto 0) is
+		
+			--when "0000000" => VALUE := "000";
+			--when "0000001" => VALUE := "001";
+			--when "0000011" => VALUE := "010";
+			--when "0000111" => VALUE := "011";
+			--when "0001111" => VALUE := "100";
+			--when "0011111" => VALUE := "101";
+			--when "0111111" => VALUE := "110";
+			--when "1111111" => VALUE := "111";						
+			--when others    => VALUE := "111";
+						
+			when "0000000" => VALUE := "000";
+			
+			when "0000001" => VALUE := "001";
+			
+			when "0000010" => VALUE := "001";
+			when "0000011" => VALUE := "010";
+			
+			when "0000100" => VALUE := "000";
+			when "0000101" => VALUE := "001";
+			when "0000110" => VALUE := "010";
+			when "0000111" => VALUE := "011";
+			
+			when "0001000" => VALUE := "000";
+			when "0001001" => VALUE := "001";
+			when "0001010" => VALUE := "010";
+			when "0001011" => VALUE := "011";
+			when "0001100" => VALUE := "100";
+			when "0001101" => VALUE := "100";
+			when "0001110" => VALUE := "100";
+			when "0001111" => VALUE := "100";
+			
+			when "0010000" => VALUE := "000";
+			when "0010001" => VALUE := "001";
+			when "0010010" => VALUE := "001";
+			when "0010011" => VALUE := "010";
+			when "0010100" => VALUE := "010";
+			when "0010101" => VALUE := "011";
+			when "0010110" => VALUE := "100";
+			when "0010111" => VALUE := "100";
+			when "0011000" => VALUE := "100";
+			when "0011001" => VALUE := "100";
+			when "0011010" => VALUE := "100";
+			when "0011011" => VALUE := "101";
+			when "0011100" => VALUE := "101";
+			when "0011101" => VALUE := "101";
+			when "0011110" => VALUE := "101";
+			when "0011111" => VALUE := "101";
+			
+			when "0100000" => VALUE := "000";
+			when "0100001" => VALUE := "001";
+			when "0100010" => VALUE := "001";
+			when "0100011" => VALUE := "010";
+			when "0100100" => VALUE := "010";
+			when "0100101" => VALUE := "011";
+			when "0100110" => VALUE := "011";
+			when "0100111" => VALUE := "011";
+			when "0101000" => VALUE := "011";
+			when "0101001" => VALUE := "100";
+			when "0101010" => VALUE := "100";
+			when "0101011" => VALUE := "100";
+			when "0101100" => VALUE := "100";
+			when "0101101" => VALUE := "100";
+			when "0101110" => VALUE := "100";
+			when "0101111" => VALUE := "100";
+			when "0110000" => VALUE := "100";
+			when "0110001" => VALUE := "101";
+			when "0110010" => VALUE := "101";
+			when "0110011" => VALUE := "101";
+			when "0110100" => VALUE := "101";
+			when "0110101" => VALUE := "101";
+			when "0110110" => VALUE := "000";
+			when "0110111" => VALUE := "110";
+			when "0111000" => VALUE := "110";
+			when "0111001" => VALUE := "110";
+			when "0111010" => VALUE := "110";
+			when "0111011" => VALUE := "110";
+			when "0111100" => VALUE := "110";
+			when "0111101" => VALUE := "110";
+			when "0111110" => VALUE := "110";
+			when "0111111" => VALUE := "110";
+			
+			when "1000000" => VALUE := "000";
+			when "1000001" => VALUE := "001";
+			when "1000010" => VALUE := "001";
+			when "1000011" => VALUE := "010";
+			when "1000100" => VALUE := "010";
+			when "1000101" => VALUE := "010";
+			when "1000110" => VALUE := "011";
+			when "1000111" => VALUE := "011";
+			when "1001000" => VALUE := "010";
+			when "1001001" => VALUE := "011";
+			when "1001010" => VALUE := "011";
+			when "1001011" => VALUE := "100";
+			when "1001100" => VALUE := "011";
+			when "1001101" => VALUE := "100";
+			when "1001110" => VALUE := "100";
+			when "1001111" => VALUE := "110";
+			when "1010000" => VALUE := "010";
+			when "1010001" => VALUE := "011";
+			when "1010010" => VALUE := "011";
+			when "1010011" => VALUE := "100";
+			when "1010100" => VALUE := "011";
+			when "1010101" => VALUE := "010";
+			when "1010110" => VALUE := "100";
+			when "1010111" => VALUE := "110";
+			when "1011000" => VALUE := "100";
+			when "1011001" => VALUE := "100";
+			when "1011010" => VALUE := "100";
+			when "1011011" => VALUE := "110";
+			when "1011100" => VALUE := "100";
+			when "1011101" => VALUE := "110";
+			when "1011110" => VALUE := "110";
+			when "1011111" => VALUE := "111";
+			when "1100000" => VALUE := "010";
+			when "1100001" => VALUE := "011";
+			when "1100010" => VALUE := "100";
+			when "1100011" => VALUE := "100";
+			when "1100100" => VALUE := "100";
+			when "1100101" => VALUE := "100";
+			when "1100110" => VALUE := "100";
+			when "1100111" => VALUE := "110";
+			when "1101000" => VALUE := "011";
+			when "1101001" => VALUE := "100";
+			when "1101010" => VALUE := "100";
+			when "1101011" => VALUE := "110";
+			when "1101100" => VALUE := "100";
+			when "1101101" => VALUE := "110";
+			when "1101110" => VALUE := "110";
+			when "1101111" => VALUE := "111";
+			when "1110000" => VALUE := "011";
+			when "1110001" => VALUE := "100";
+			when "1110010" => VALUE := "100";
+			when "1110011" => VALUE := "110";
+			when "1110100" => VALUE := "100";
+			when "1110101" => VALUE := "110";
+			when "1110110" => VALUE := "110";
+			when "1110111" => VALUE := "111";
+			when "1111000" => VALUE := "110";
+			when "1111001" => VALUE := "110";
+			when "1111010" => VALUE := "110";
+			when "1111011" => VALUE := "111";
+			when "1111100" => VALUE := "111";
+			when "1111101" => VALUE := "111";
+			when "1111110" => VALUE := "111";
+			when "1111111" => VALUE := "111";			
+
+		end case;
+		
+		return VALUE;
+		
+end F_ADC;
+
 begin
 
 red: process(CLOCK_H, DAC_STEP, ARGB(2))
 begin
 	if (rising_edge(CLOCK_H)) then 
 		captureR <= '1';
-		if (DAC_STEP = "000" or ARGB(2) /= 'X') then
 		case DAC_STEP is		
 			when "001" => 
 				RADC(0) <= ARGB(2);
@@ -60,32 +218,9 @@ begin
 			when "111" => 
 				RADC(6) <= ARGB(2);
 			when "000" =>
-				case RADC is
-						when "0000000" => PIXEL(7 downto 5) <= "000";
-						when "0000001" => PIXEL(7 downto 5) <= "001";
-						when "0000011" => PIXEL(7 downto 5) <= "010";
-						when "0000111" => PIXEL(7 downto 5) <= "011";
-						when "0001111" => PIXEL(7 downto 5) <= "100";
-						when "0011111" => PIXEL(7 downto 5) <= "101";
-						when "0111111" => PIXEL(7 downto 5) <= "110";
-						when "1111111" => PIXEL(7 downto 5) <= "111";
-
-						when "-------" => PIXEL(7 downto 5) <= "111";
-						when "------1" => PIXEL(7 downto 5) <= "001";
-						when "-----1-" => PIXEL(7 downto 5) <= "010";
-						when "----1--" => PIXEL(7 downto 5) <= "011";
-						when "---1---" => PIXEL(7 downto 5) <= "100";
-						when "--1----" => PIXEL(7 downto 5) <= "101";
-						when "-1-----" => PIXEL(7 downto 5) <= "110";
-						when "1------" => PIXEL(7 downto 5) <= "111";
-
-
-						when others 	=> PIXEL(7 downto 5) <= "111";
-				end case;
-				RADC <= (others => '0');
+				PIXEL(7 downto 5) <= F_ADC(RADC);
 				captureR <= '0';
 		end case;
-		end if;
 	end if;
 end process;
 
@@ -93,7 +228,6 @@ green: process(CLOCK_H, DAC_STEP, ARGB(1))
 begin
 	if (rising_edge(CLOCK_H)) then 
 		captureG <= '1';
-		if (DAC_STEP = "000" or ARGB(1) /= 'X') then		
 		case DAC_STEP is		
 			when "001" => 
 				GADC(0) <= ARGB(1);
@@ -110,32 +244,9 @@ begin
 			when "111" => 
 				GADC(6) <= ARGB(1);
 			when "000" =>
-				case GADC is
-						when "0000000" => PIXEL(4 downto 2) <= "000";
-						when "0000001" => PIXEL(4 downto 2) <= "001";
-						when "0000011" => PIXEL(4 downto 2) <= "010";
-						when "0000111" => PIXEL(4 downto 2) <= "011";
-						when "0001111" => PIXEL(4 downto 2) <= "100";
-						when "0011111" => PIXEL(4 downto 2) <= "101";
-						when "0111111" => PIXEL(4 downto 2) <= "110";
-						when "1111111" => PIXEL(4 downto 2) <= "111";
-						 
-						when "-------" => PIXEL(4 downto 2) <= "111";
-						when "------1" => PIXEL(4 downto 2) <= "001";
-						when "-----1-" => PIXEL(4 downto 2) <= "010";
-						when "----1--" => PIXEL(4 downto 2) <= "011";
-						when "---1---" => PIXEL(4 downto 2) <= "100";
-						when "--1----" => PIXEL(4 downto 2) <= "101";
-						when "-1-----" => PIXEL(4 downto 2) <= "110";
-						when "1------" => PIXEL(4 downto 2) <= "111";						
-						
-						when others 	=> PIXEL(4 downto 2) <= "111";
-						
-				end case;
-				GADC <= (others => '0');				
+				PIXEL(4 downto 2) <= F_ADC(GADC);
 				captureG <= '0';				
 		end case;
-		end if;		
 	end if;
 end process;
 
@@ -143,7 +254,6 @@ blue: process(CLOCK_H, DAC_STEP, ARGB(0))
 begin
 	if (rising_edge(CLOCK_H)) then 
 		captureB <= '1'; 	
-		if (DAC_STEP = "000" or ARGB(0) /= 'X') then		
 		case DAC_STEP is		
 			when "001" => 
 				BADC(0) <= ARGB(0);
@@ -160,31 +270,9 @@ begin
 			when "111" => 
 				BADC(6) <= ARGB(0);
 			when "000" =>
-				case BADC is
-						when "0000000" => PIXEL(1 downto 0) <= "00";
-						when "0000001" => PIXEL(1 downto 0) <= "00";
-						when "0000011" => PIXEL(1 downto 0) <= "01";
-						when "0000111" => PIXEL(1 downto 0) <= "01";
-						when "0001111" => PIXEL(1 downto 0) <= "10";
-						when "0011111" => PIXEL(1 downto 0) <= "10";
-						when "0111111" => PIXEL(1 downto 0) <= "11";
-						when "1111111" => PIXEL(1 downto 0) <= "11";
-						
-						when "-------" => PIXEL(1 downto 0) <= "11";
-						when "------1" => PIXEL(1 downto 0) <= "00";
-						when "-----1-" => PIXEL(1 downto 0) <= "01";
-						when "----1--" => PIXEL(1 downto 0) <= "01";
-						when "---1---" => PIXEL(1 downto 0) <= "10";
-						when "--1----" => PIXEL(1 downto 0) <= "10";
-						when "-1-----" => PIXEL(1 downto 0) <= "11";
-						when "1------" => PIXEL(1 downto 0) <= "11";						
-						
-						when others 	=> PIXEL(1 downto 0) <= "11";
-				end case;
-				BADC <= (others => '0');
+				PIXEL(1 downto 0) <= F_ADC(BADC)(2 downto 1);
 				captureB <= '0';
 		end case;
-		end if;
 	end if;
 end process;
 
@@ -199,7 +287,7 @@ begin
 		end if;
 				
 		IS_SYNC <= '0';
-		--if (VSYNC_IN /= SYNC_LEVEL and hcount_in(31 downto 3) > 1024) then
+		--if (VSYNC_IN /= SYNC_LEVEL and hcount_in(31 downto 3) > 8192) then
 		--	IS_SYNC <= '1';
 		--end if;
 		
